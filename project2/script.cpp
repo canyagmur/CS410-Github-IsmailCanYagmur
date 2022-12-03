bc04554feff7ecfedecc20a56444bc78971091e2
canyagmur <can.yagmur@ozu.edu.tr>
2022-12-03 20:06:15 +0300

    init project

---
+++ project2/script.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    // if (argc < 2)
+    // {
+    //     // print usage
+
+    //     std::cerr << "[ERR]Expected filename as an argument.\n[INFO]Exiting... " << std::endl;
+    //     return 1;
+    // }
+    // std::string file_path = argv[1];
+
+    return 0;
+}
