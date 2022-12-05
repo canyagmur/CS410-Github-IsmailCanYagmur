29c215ece6dfc5e4d039433659e0944521dc9f91
canyagmur <can.yagmur@ozu.edu.tr>
2022-12-05 22:23:51 +0300

    epsilon rule is completed

--- project2/script.cpp
+++ project2/script.cpp
@@ -77,6 +77,18 @@ auto FileReader(string file_path){
             }
             return cfg; }
 
+auto print_rules(CFG cfg){
+    for(auto rule : cfg.RULES)
+        {
+            cout<<rule.first<<"->";
+            for(auto rule_rhs : rule.second)
+            {
+                cout<<rule_rhs<<"|";
+            }
+            cout<<endl;
+        }
+}
+
 
 vector<int> findLocation(string sample, char findIt)
 {
@@ -88,7 +100,7 @@ vector<int> findLocation(string sample, char findIt)
     return characterLocations;
 }
 
-void permute_recursive(set<string> &permutations,vector<int> indexes_of_key_variables, int index_counter)
+void permute_recursive(set<string> &permutations,set<string> local_permutations,vector<int> indexes_of_key_variables, int index_counter)
 {
     if(index_counter == indexes_of_key_variables.size())
         {
@@ -101,32 +113,108 @@ void permute_recursive(set<string> &permutations,vector<int> indexes_of_key_vari
             temp[indexes_of_key_variables[index_counter]] ='E';
             permutations.insert(temp);
             permutations.insert(permuted_string);
-            permute_recursive(permutations,indexes_of_key_variables,index_counter+1);
+            local_permutations.clear();
+            local_permutations.insert(temp);
+            local_permutations.insert(permuted_string);
+            permute_recursive(permutations,local_permutations,indexes_of_key_variables,index_counter+1);
         }
 
     }
 }
-auto remove_epsilon_rules(string key_variable,string string_containing_key_variable) 
+auto update_rule_by_removing_epsilon(string key_variable,string string_containing_key_variable) 
 {
+
     // if A->e, then key_variable is A and string_containing_key_variable is a string containing the key variable in RHS of the rules.
     char findIt = key_variable[0]; //key variable is assumed to 
     auto indexes_of_key_variables_in_string = findLocation(string_containing_key_variable,findIt);
-    //cout<<indexes_of_key_variables_in_string.size()+1<<endl;
-    set<string> all_permutations_temp;
-    all_permutations_temp.insert(string_containing_key_variable);
-    permute_recursive(all_permutations_temp,indexes_of_key_variables_in_string,0);
     set<string> all_permutations;
-    for(auto str : all_permutations_temp)
-    {
-        str.erase(std::remove(str.begin(), str.end(), 'E'), str.end());
-        all_permutations.insert(str);
+    if(indexes_of_key_variables_in_string.size() == string_containing_key_variable.size()){
+        all_permutations.insert("e");
     }
-    for(auto str : all_permutations)
-    {
-        cout<<str<<endl;
+    else{
+        //cout<<indexes_of_key_variables_in_string.size()+1<<endl;
+        set<string> all_permutations_temp;
+        all_permutations_temp.insert(string_containing_key_variable);
+        permute_recursive(all_permutations_temp,all_permutations_temp,indexes_of_key_variables_in_string,0);
+        
+        for(auto str : all_permutations_temp)
+        {
+            str.erase(std::remove(str.begin(), str.end(), 'E'), str.end());
+            all_permutations.insert(str);
+        }
+
     }
+    // for(auto str : all_permutations)
+    //     {
+    //         cout<<str<<endl;
+    //     }
+
     return all_permutations;
 
+
+}
+
+
+auto search_rules_for_epsilon(CFG cfg)
+{
+
+    set<string> rules_containing_epsilon;
+    for(auto rule : cfg.RULES)
+    {
+        for(auto rule_rhs : rule.second)
+        {
+            if(rule_rhs == "e")
+                rules_containing_epsilon.insert(rule.first);
+        }
+    }
+    return rules_containing_epsilon;
+}
+
+
+auto make_cfg_in_cnf(CFG cfg, vector<string> letters)
+{
+
+cout<<"----------------------------Rules of the original CFG----------------------------"<<endl;
+print_rules(cfg);
+
+ auto cfg_in_cnf = cfg;
+
+ //1. Remove all the rules containing epsilon
+ //set<string> rules_containing_epsilon;
+while(1)
+{
+    auto rules_containing_epsilon = search_rules_for_epsilon(cfg_in_cnf);
+    if(rules_containing_epsilon.empty())
+    {
+        break;
+    }
+    else
+    {
+        for(auto rule_with_empty_string : rules_containing_epsilon)
+        {
+            auto other_rules = cfg_in_cnf.RULES;
+            other_rules.erase(rule_with_empty_string);
+            for(auto r : other_rules){
+                for(auto rule_rhs : r.second)
+                {
+                    if(rule_rhs.find(rule_with_empty_string) != std::string::npos) //if the other_rule_rhs contains the rule_with_empty_string 
+                    {
+                        auto new_rules = update_rule_by_removing_epsilon(rule_with_empty_string,rule_rhs);
+                        cfg_in_cnf.RULES[r.first].erase(rule_rhs);
+                        cfg_in_cnf.RULES[r.first].insert(new_rules.begin(),new_rules.end());
+                    }
+                }
+            }
+            cfg_in_cnf.RULES[rule_with_empty_string].erase("e");
+        }
+    }
+}
+
+cout<<"----------------------------Rules of the CFG AFTER Epsilon Rule----------------------------"<<endl;
+print_rules(cfg_in_cnf);
+
+
+ return cfg_in_cnf;
 
 }
 
@@ -147,8 +235,24 @@ int main(int argc, char *argv[])
     }
 
 
+
+
     try {
-        auto cfg = FileReader("G1.txt");
+        auto cfg = FileReader("G2.txt");
+
+        //remove all letters coming from cfg definition from the letters pool.
+        for(auto a : cfg.NON_TERMINAL){
+                LETTER_POOL.erase(remove(LETTER_POOL.begin(), LETTER_POOL.end(), a), LETTER_POOL.end());
+            }
+        for(auto a : cfg.TERMINAL){
+                LETTER_POOL.erase(remove(LETTER_POOL.begin(), LETTER_POOL.end(), a), LETTER_POOL.end());
+        }
+        LETTER_POOL.erase(remove(LETTER_POOL.begin(), LETTER_POOL.end(), cfg.START), LETTER_POOL.end());
+        //remove all letters coming from cfg definition from the letters pool.
+
+
+        auto cfg_in_cnf = make_cfg_in_cnf(cfg,LETTER_POOL);
+
 
         // for(const auto& elem : cfg.RULES)
         // {
